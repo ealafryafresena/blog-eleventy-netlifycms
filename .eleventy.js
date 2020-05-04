30f1f71c077d2d38ed36d60c629e98e8af55f66c
Claus Rux <claus.rux@gmx.de>
2020-05-04 15:01:40 +0200

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("img");
+
+  return {
+    passthroughFileCopy: true,
+    markdownTemplateEngine: "njk",
+    templateFormats: ["html", "njk", "md"],
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
